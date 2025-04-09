f66e93fb163abaf4c54af9c9426b585278b1103b
christian turner <cturner@ottawacloudconsulting.com>
2025-04-08 20:56:33 -0400

    fix - job spec backoff limit

--- src/trigger/main.py
+++ src/trigger/main.py
@@ -62,7 +62,7 @@ def trigger_sync(response: Response):
             ),
             spec=client.V1JobSpec(
                 template=cronjob.spec.job_template,
-                backoff_limit=cronjob.spec.backoff_limit or 1
+                backoff_limit=cronjob.spec.job_template.spec.backoff_limit or 1
             ),
 
         )
